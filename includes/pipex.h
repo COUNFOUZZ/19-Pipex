dfc938f38642f6f28edc898051e39dbcf1aa5f04
Alexis Abda <aabda@student.s19.be>
2022-09-22 18:14:17 +0200

    add structure

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 11:01:49 by aabda             #+#    #+#             */
-/*   Updated: 2022/09/18 21:42:09 by aabda            ###   ########.fr       */
+/*   Updated: 2022/09/22 15:45:55 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,18 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+
+typedef struct s_data
+{
+	int		infile;
+	int		outfile;
+	int		pid_1;
+	int		pid_2;
+	int		status;
+	int		fd_pipe[2];
+	char	*cmd1;
+	char	*cmd2;
+}	t_data;
 
 size_t	ft_strlen(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
