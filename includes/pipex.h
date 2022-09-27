3066835303e54faedf6c6e0720c57e0e2eb336ae
Alexis Abda <aabda@student.s19.be>
2022-09-27 19:54:58 +0200

    Add function getcmd and add some variables in the structure

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 11:01:49 by aabda             #+#    #+#             */
-/*   Updated: 2022/09/22 15:45:55 by aabda            ###   ########.fr       */
+/*   Updated: 2022/09/27 18:10:36 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,10 @@ typedef struct s_data
 	int		pid_2;
 	int		status;
 	int		fd_pipe[2];
-	char	*cmd1;
-	char	*cmd2;
+	char	*path_cmd1;
+	char	*path_cmd2;
+	char	**cmd1;
+	char	**cmd2;
 }	t_data;
 
 size_t	ft_strlen(const char *s);
@@ -34,5 +36,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*cmd_exist(char	*cmd, char **paths);
 char	**ft_split(char const *s, char c);
+char	**getcmd(char *s);
 
 #endif
