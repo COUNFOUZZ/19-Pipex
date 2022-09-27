4edff5c959d506060f8285cdb52d298fab60cef0
Alexis Abda <aabda@student.s19.be>
2022-09-27 19:56:32 +0200

    fix some problems in test.c

--- srcs/test.c
+++ srcs/test.c
@@ -4,59 +4,52 @@
 #include <unistd.h>
 #include "../includes/pipex.h"
 
-void	ft_putstr(const char *s)
-{
-	if (!s)
-		return ;
-	write(1, s, ft_strlen(s));
-}
-
 int	main(int argc, char **argv, char **envp)
 {
 	t_data data;
-	char *test[] =  {"ls", "-la", NULL};
-	char *test2[] =  {"wc", NULL, NULL};
-
+	data.cmd1 = getcmd(argv[2]);
+	data.cmd2 = getcmd(argv[3]);
 	data.infile = open(argv[1], O_RDONLY);
 	data.outfile = open(argv[4], O_RDWR | O_TRUNC | O_CREAT, 0644);
-	pipe(data.fd_pipe);
+	if (pipe(data.fd_pipe) == -1)
+		return (1);
 	data.pid_1 = fork();
 	if (data.pid_1 == -1)
 	{
-		ft_putstr("[CHILD-1]\n");
-		ft_putstr("Error creating process");
+		printf("[CHILD-1]\n");
+		printf("Error creating process");
 		return (1);
 	}
 	else if (data.pid_1 == 0)
 	{
-		ft_putstr("[CHILD-1]\n");
+		// printf("[CHILD-1]\n");
 		dup2(data.infile, STDIN_FILENO);
 		dup2(data.fd_pipe[1], STDOUT_FILENO);
 		close(data.fd_pipe[0]);
 		close(data.fd_pipe[1]);
-		data.cmd1 = cmd_exist(argv[2], envp);
-		execve(data.cmd1, test, NULL);
+		data.path_cmd1 = cmd_exist(data.cmd1[0], envp);
+		execve(data.path_cmd1, data.cmd1, NULL);
 	}
 	else if (data.pid_1 > 0)
 	{
 		data.pid_2 = fork();
 		if (data.pid_2 == -1)
 		{
-			ft_putstr("[CHILD-2]\n");
-			ft_putstr("Error creating process");
+			// printf("[CHILD-2]\n");
+			printf("Error creating process");
 			return (1);
 		}
 		if (data.pid_2 == 0)
 		{
-			ft_putstr("[CHILD-2]\n");
+			// printf("[CHILD-2]\n");
 			dup2(data.fd_pipe[0], STDIN_FILENO);
 			dup2(data.outfile, STDOUT_FILENO);
 			close(data.fd_pipe[0]);
 			close(data.fd_pipe[1]);
-			data.cmd2 = cmd_exist(argv[3], envp);
-			execve(data.cmd2, test2, NULL);
+			data.path_cmd2 = cmd_exist(data.cmd2[0], envp);
+			execve(data.path_cmd2, data.cmd2, NULL);
 		}
-		ft_putstr("[PARENT]\n");
+		// printf("[PARENT]\n");
 		close(data.fd_pipe[0]);
 		close(data.fd_pipe[1]);
 		close(data.infile);
