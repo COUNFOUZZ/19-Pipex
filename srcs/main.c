8307e27526f8097e9cdeb152969104bb982483fe
Alexis Abda <aabda@student.s19.be>
2022-09-28 17:44:19 +0200

    use function print_error

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,11 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 13:55:58 by aabda             #+#    #+#             */
-/*   Updated: 2022/09/28 14:14:40 by aabda            ###   ########.fr       */
+/*   Updated: 2022/09/28 17:42:04 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
-#include <stdio.h>	// need to be delete
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -22,14 +21,14 @@ int	main(int argc, char **argv, char **envp)
 		return (1);
 	data.pid_1 = fork();
 	if (data.pid_1 == -1)
-		return (printf("Error creating process"));	// need to not use printf
+		print_error();
 	if (data.pid_1 == 0)
 		child1(data, envp);
 	else if (data.pid_1 > 0)
 	{
 		data.pid_2 = fork();
 		if (data.pid_2 == -1)
-			return (printf("Error creating process"));	// need to not use printf
+			print_error();
 		if (data.pid_2 == 0)
 			child2(data, envp);
 		parent(data);
