ab1bf7f194744c9bc5416a750bcb1a9c74382edc
Alexis Abda <aabda@student.s19.be>
2022-09-28 18:16:10 +0200

    add error handling

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 16:13:12 by aabda             #+#    #+#             */
-/*   Updated: 2022/09/28 17:39:24 by aabda            ###   ########.fr       */
+/*   Updated: 2022/09/28 18:11:19 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,21 @@ char	**getcmd(char *s)
 	return (ft_split(s, ' '));
 }
 
-void	initialize_struct(t_data *data, char **argv)
-{
-	data->cmd1 = getcmd(argv[2]);
-	data->cmd2 = getcmd(argv[3]);
-	data->infile = open(argv[1], O_RDONLY);
-	data->outfile = open(argv[4], O_RDWR | O_TRUNC | O_CREAT, 0644);
-}
-
 void	print_error(void)
 {
 	write(2, strerror(errno), ft_strlen(strerror(errno)));
 	write(2, "\n", 1);
 	exit(1);
+}
+
+void	initialize_struct(t_data *data, char **argv)
+{
+	data->cmd1 = getcmd(argv[2]);
+	data->cmd2 = getcmd(argv[3]);
+	data->infile = open(argv[1], O_RDONLY);
+	if (data->infile == -1)
+		print_error();
+	data->outfile = open(argv[4], O_RDWR | O_TRUNC | O_CREAT, 0644);
+	if (data->outfile == -1)
+		print_error();
 }
