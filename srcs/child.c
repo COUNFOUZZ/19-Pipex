664bfa022c3238f329ea8401c60588fb892bf444
Alexis Abda <alexis.abda@hotmail.com>
2022-10-10 14:45:00 +0200

    fix leaks with data.cmd1 and data.cmd2

--- srcs/child.c
+++ srcs/child.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 11:11:05 by aabda             #+#    #+#             */
-/*   Updated: 2022/09/29 13:58:15 by aabda            ###   ########.fr       */
+/*   Updated: 2022/10/10 14:43:53 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,10 +36,20 @@ void	child2(t_data data, char **envp)
 
 void	parent(t_data data)
 {
+	int	i;
+
 	ft_error(close(data.fd_pipe[0]));
 	ft_error(close(data.fd_pipe[1]));
 	ft_error(close(data.infile));
 	ft_error(close(data.outfile));
 	ft_error(waitpid(data.pid_1, &data.status, 0));
 	ft_error(waitpid(data.pid_2, &data.status, 0));
+	i = -1;
+	while (data.cmd1[++i])
+		free(data.cmd1[i]);
+	free(data.cmd1);
+	i = -1;
+	while (data.cmd2[++i])
+		free(data.cmd2[i]);
+	free(data.cmd2);
 }
